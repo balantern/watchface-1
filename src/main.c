08046e71b423e88be16afe791886808c3ef69f22
Julie Kim <jewel2000k@yahoo.com>
2016-07-19 11:27:52 -0500

    reversion to previous commit
    commit saved to wrong repo

--- src/main.c
+++ src/main.c
@@ -1,33 +1,102 @@
-#include <pebble.h>
-#include "main.h"
+ #include <pebble.h>
 
-Window *window;
-TextLayer *text_layer;
-BitmapLayer *background_layer;
+static Window *s_main_window;
+static TextLayer *s_time_layer;  
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
-void window_load(Window *window) {
-	background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-	bitmap_layer_set_background_color(background_layer, GColorMintGreen);
-	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(background_layer));
-	
-	text_layer = text_layer_create(GRect(0, 30, 144, 20));
-	text_layer_set_text(text_layer, "placeholder");
-	text_layer_set_text_color(text_layer, GColorFolly);
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // format the way that the time text is going to be displayed
+strftime(buffer, sizeof("00:00"),"%I%M", tick_time);
+
+  // Display this time on the TextLayer
+	text_layer_set_text(s_time_layer, buffer);
 }
 
-void window_unload(Window *window) {
-	text_layer_destroy(text_layer);
-	bitmap_layer_destroy(background_layer);
+
+static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SPARK_ICON);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 140, 180));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
+  // Create time TextLayer
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ATE_BIT_24));
+
+  //time layer
+  s_time_layer = text_layer_create(GRect(12, 14, 120, 25));
+  text_layer_set_background_color(s_time_layer, GColorBlueMoon);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
-int main() {
-	window = window_create();
-	window_set_window_handlers(window, (WindowHandlers){
-		.load = window_load,
-		.unload = window_unload
-	});
-	window_stack_push(window, true);
-	app_event_loop();
+static void main_window_unload(Window *window) {
+    
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+    // Destroy time layer
+    text_layer_destroy(s_time_layer);
+    
+}
+
+//register all events
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+ update_time();
+ 
+}
+
+  
+
+
+static void init() {
+ 
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Make sure the time is displayed from the start
+  update_time();
+}
+
+static void deinit() {
+    // Destroy Window
+    window_destroy(s_main_window);
+}
+
+
+//this is the main road map for the app  first it will initialize by running init(), then it will run the app until
+//the user exits the app and then will run the deinit() code, which will free up all of the memory that we 
+//set aside at the for the app.
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }
